728c6531a7677dea63f50face0a87501f4bbeb92
Helen Koike <helen.koike@collabora.com>
2017-05-29 18:00:59 -0300

    tmp: fix build with setuptools

--- build_packages.sh
+++ build_packages.sh
@@ -49,9 +49,9 @@ function build_distro() {
     --maintainer 'gc-team@google.com' \
     --name "${name}" \
     --no-python-fix-name \
-    --python-install-bin '%%PREFIX%%/bin' \
+    --python-install-bin '/usr/bin' \
     --python-install-lib "${py_path}" \
-    --python-install-data "%%PREFIX%%/share/doc/${name}" \
+    --python-install-data "/usr/share/doc/${name}" \
     --rpm-dist "${distro}" \
     setup.py
 }
@@ -83,9 +83,6 @@ fi
 
 for build in "${BUILD[@]}"; do
   case "$build" in
-    freebsd) # FreeBSD
-      build_distro 'freebsd' 'dir' '%%PREFIX%%/lib/python2.7/site-packages'
-      ;;
     el6) # RHEL/CentOS 6
       build_distro 'el6' 'rpm' '/usr/lib/python2.6/site-packages'
       ;;
--- setup.py
+++ setup.py
@@ -30,7 +30,6 @@ setuptools.setup(
     long_description='Google Compute Engine guest environment.',
     name='google-compute-engine',
     packages=setuptools.find_packages(),
-    scripts=glob.glob('scripts/*'),
     url='https://github.com/GoogleCloudPlatform/compute-image-packages',
     version='2.3.7',
     # Entry points create scripts in /usr/bin that call a function.
