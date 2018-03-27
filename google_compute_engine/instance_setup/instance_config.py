135bafb5c77af305acf43a53c1dd44fb6df011aa
Helen Koike <helen.koike@collabora.com>
2018-03-27 13:49:15 +0000

    FreeBSD: disable scripts/*
    Those scripts apply for Linux only

--- google_compute_engine/instance_setup/instance_config.py
+++ google_compute_engine/instance_setup/instance_config.py
@@ -74,11 +74,11 @@ class InstanceConfig(config_manager.ConfigManager):
       },
       'InstanceSetup': {
           'host_key_types': 'ecdsa,ed25519,rsa',
-          'optimize_local_ssd': 'true',
+          'optimize_local_ssd': 'false',
           'network_enabled': 'true',
           'set_boto_config': 'true',
           'set_host_keys': 'true',
-          'set_multiqueue': 'true',
+          'set_multiqueue': 'false',
       },
       'IpForwarding': {
           'ethernet_proto_id': '66',
--- setup.py
+++ setup.py
@@ -34,7 +34,6 @@ setuptools.setup(
     long_description='Google Compute Engine guest environment.',
     name='google-compute-engine',
     packages=setuptools.find_packages(),
-    scripts=glob.glob('scripts/*'),
     url='https://github.com/GoogleCloudPlatform/compute-image-packages',
     version='2.8.3',
     # Entry points create scripts in /usr/bin that call a function.
