a2aeca32e2416200c2fc8a61a51c922dd6fe31f6
Helen Koike <helen.koike@collabora.com>
2018-06-08 00:43:57 -0300

    tmp: network test

--- google_compute_engine/networking/ip_forwarding/ip_forwarding_utils.py
+++ google_compute_engine/networking/ip_forwarding/ip_forwarding_utils.py
@@ -17,10 +17,120 @@
 
 import re
 import subprocess
+import netifaces
+import netaddr
 
 IP_REGEX = re.compile(r'\A(\d{1,3}\.){3}\d{1,3}\Z')
 IP_ALIAS_REGEX = re.compile(r'\A(\d{1,3}\.){3}\d{1,3}/\d{1,2}\Z')
 
+
+class IpForwardingUtilsFreeBSD(object):
+  """System IP address configuration utilities."""
+
+  def __init__(self, logger, proto_id=None):
+    """Constructor.
+
+    Args:
+      logger: logger object, used to write to SysLog and serial port.
+      proto_id: string, the routing protocol identifier for Google IP changes.
+    """
+    self.logger = logger
+    self.proto_id = proto_id or '66'
+
+  def _RunIfconfig(self, args=None, options=None):
+    """Run a command with ifconfig and return the response.
+
+    Args:
+      args: list, the string ip route command args to execute.
+      options: dict, the string parameters to append to the ip route command.
+
+    Returns:
+      string, the standard output from the ip route command execution.
+    """
+    args = args or []
+    options = options or {}
+    command = ['ifconfig']
+    command.extend(args)
+    for item in options.items():
+      command.extend(item)
+    try:
+      process = subprocess.Popen(
+          command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+      stdout, stderr = process.communicate()
+    except OSError as e:
+      self.logger.warning('Exception running %s. %s.', command, str(e))
+    else:
+      if process.returncode:
+        message = 'Non-zero exit status running %s. %s.'
+        self.logger.warning(message, command, stderr.strip())
+      else:
+        return stdout.decode('utf-8', 'replace')
+    return ''
+
+  def ParseForwardedIps(self, forwarded_ips):
+    """Parse and validate forwarded IP addresses.
+
+    Args:
+      forwarded_ips: list, the IP address strings to parse.
+
+    Returns:
+      list, the valid IP address strings.
+    """
+    addresses = []
+    forwarded_ips = forwarded_ips or []
+    for ip in forwarded_ips:
+      if ip and (IP_REGEX.match(ip) or IP_ALIAS_REGEX.match(ip)):
+        addresses.append(ip[:-3] if ip.endswith('/32') else ip)
+      else:
+        self.logger.warning('Could not parse IP address: "%s".', ip)
+    return addresses
+
+  def GetForwardedIps(self, interface):
+    """Retrieve the list of configured forwarded IP addresses.
+
+    Args:
+      interface: string, the output device to query.
+
+    Returns:
+      list, the IP address strings.
+    """
+    interface = 'lo%d-%s' % (self.proto_id, interface)
+    try:
+      ips = netifaces.ifaddresses(interface)
+      ips = ips[netifaces.AF_INET]
+    except (ValueError, KeyError) as e:
+      return []
+    forwarded_ips = []
+    for ip in ips:
+      forwarded_ips.append(ip['addr'] + '/' + str(netaddr.IPAddress(ip['netmask']).netmask_bits()))
+    return self.ParseForwardedIps(forwarded_ips)
+
+  def AddForwardedIp(self, address, interface):
+    """Configure a new IP address on the network interface.
+
+    Args:
+      address: string, the IP address to configure.
+      interface: string, the output device to use.
+    """
+    address = address if IP_ALIAS_REGEX.match(address) else '%s/32' % address
+    interface = 'lo%d-%s' % (self.proto_id, interface)
+    cmd = 'alias'
+    try:
+      forwarded_ips = netifaces.ifaddresses(interface)
+    except (ValueError, KeyError) as e:
+      cmd = 'create'
+    self._RunIfconfig(args=[interface, cmd, address])
+
+  def RemoveForwardedIp(self, address, interface):
+    """Delete an IP address on the network interface.
+
+    Args:
+      address: string, the IP address to configure.
+      interface: string, the output device to use.
+    """
+    address = address if IP_ALIAS_REGEX.match(address) else '%s/32' % address
+    interface = 'lo%d-%s' % (self.proto_id, interface)
+    self._RunIfconfig(args=[interface, '-alias', address])
 
 class IpForwardingUtils(object):
   """System IP address configuration utilities."""
